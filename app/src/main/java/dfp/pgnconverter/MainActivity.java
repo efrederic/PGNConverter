b8bcdfe605ef2f7f87d091caeedbb7a265f75ed6
efrederic <ethan.frederic@gmail.com>
2017-06-23 19:05:49 -0500

    Initial commit

---
+++ app/src/main/java/dfp/pgnconverter/MainActivity.java
@@ -0,0 +1,120 @@
+package dfp.pgnconverter;
+
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "DFP";
+    final int REQUEST_WRITE_ACCESS = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button droidfish = (Button) findViewById(R.id.droidfish);
+        droidfish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                savePGN("DroidFish/pgn");
+            }
+        });
+
+        Button custom = (Button) findViewById(R.id.custom);
+        custom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                savePGN("Documents/PGNs");
+            }
+        });
+    }
+
+    private void createPGN(String path, String fileName, String pgnText) {
+        FileOutputStream stream;
+
+        if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+            File extDir = Environment
+                    .getExternalStoragePublicDirectory(path);
+            if (!extDir.mkdirs()) {
+                Log.e(TAG, "Failed to make directory");
+            }
+
+            File file = new File(
+                    extDir,
+                    fileName.contains(".pgn") ? fileName : fileName + ".pgn");
+
+            try {
+                if (!file.exists() && !file.createNewFile()) {
+                    Log.e(TAG, "Failed to create file");
+                }
+                stream = new FileOutputStream(file);
+                stream.write(pgnText.getBytes());
+                stream.close();
+                Toast.makeText(this, String.format("%s sent to %s", file.getName(), path),
+                        Toast.LENGTH_SHORT).show();
+            } catch (Exception e) {
+                e.printStackTrace();
+                Toast.makeText(this, "Error saving file", Toast.LENGTH_SHORT).show();
+            }
+            ((EditText) findViewById(R.id.editText)).setText("");
+        } else {
+            Toast.makeText(this, "External media is not available", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void savePGN(final String path) {
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    REQUEST_WRITE_ACCESS);
+        } else {
+            final EditText directoryText = new EditText(MainActivity.this);
+            directoryText.setHint("File name");
+
+            new AlertDialog.Builder(MainActivity.this)
+                    .setView(directoryText)
+                    .setPositiveButton("Save", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            createPGN(path, directoryText.getText().toString(),
+                                    ((EditText) findViewById(R.id.editText)).getText().toString());
+                        }
+                    })
+                    .show();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_WRITE_ACCESS: {
+                if (grantResults.length == 0 || grantResults[0]
+                        == PackageManager.PERMISSION_DENIED) {
+                    Toast.makeText(this, "Write permission required to save PGN files",
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+}
